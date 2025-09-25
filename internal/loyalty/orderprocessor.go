b8e3071cb6dbf6fc953398f0ca1aaad925d0a46f
User <email@mail.com>
2025-09-26 00:18:40 +0300

    refactor worker logic

--- internal/loyalty/orderprocessor.go
+++ internal/loyalty/orderprocessor.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"log"
+	"sync/atomic"
 	"time"
 
 	bm "github.com/skdiver33/gophermart/internal/balance"
@@ -32,8 +33,14 @@ func (processor *OrderProcessor) Start(ctx context.Context) {
 	go processor.ProcessOrder(ctx)
 }
 
+const defaultPause = 1
+
+var workerPause = int64(defaultPause)
+var workerCounter = int64(0)
+var numWorkers int64
+
 func (processor *OrderProcessor) ProcessOrder(ctx context.Context) {
-	numWorkers := 5
+
 	workerCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
 
 	select {
@@ -43,12 +50,19 @@ func (processor *OrderProcessor) ProcessOrder(ctx context.Context) {
 		return
 	default:
 		for {
-			orderInProc, _ := processor.OrderManager.GetAllUnprocOrders(ctx)
+			orderInProc, err := processor.OrderManager.GetAllUnprocOrders(ctx)
+			if err != nil {
+				log.Printf("error get unproc orders %s", err.Error())
+				cancel()
+				return
+			}
 			numJobs := len(orderInProc)
 			jobs := make(chan string, numJobs)
 			result := make(chan ProcessResult, numJobs)
 
-			for i := 0; i < numWorkers; i++ {
+			numWorkers = int64(numJobs)/5 + 1
+
+			for i := 0; i < int(numWorkers); i++ {
 				go worker(workerCtx, processor.client, jobs, result)
 			}
 			for _, order := range orderInProc {
@@ -86,34 +100,45 @@ func (processor *OrderProcessor) ProcessOrder(ctx context.Context) {
 
 func worker(ctx context.Context, client *AccrualClient, jobs <-chan string, res chan<- ProcessResult) {
 	for j := range jobs {
-		select {
-		case <-ctx.Done():
-			log.Printf("worker end work because close main ctx")
-			return
-		default:
-			for {
+
+	worker_loop:
+		for {
+			timer := time.NewTimer(time.Duration(workerPause) * time.Second)
+			if atomic.LoadInt64(&workerPause) != defaultPause {
+				if atomic.AddInt64(&workerCounter, 1) == numWorkers {
+					atomic.StoreInt64(&workerPause, defaultPause)
+					atomic.StoreInt64(&workerCounter, 1)
+				}
+			}
+			select {
+			case <-ctx.Done():
+				timer.Stop()
+				log.Printf("worker end work because close main ctx")
+				res <- ProcessResult{}
+				return
+			case <-timer.C:
 				accrualResult, err := client.GetAccural(j)
 				procResult := ProcessResult{}
 				if err != nil {
 					procResult.ProcessingError = err
 					procResult.Accrual.Order = j
 					res <- procResult
-					break
+					break worker_loop
 				}
 				if accrualResult.RetryInterval != 0 {
-					time.Sleep(time.Second * time.Duration(accrualResult.RetryInterval))
-					continue
+					atomic.CompareAndSwapInt64(&workerPause, defaultPause, int64(accrualResult.Accrual))
+					break
 				}
 				if accrualResult.Status == "PROCESSING" || accrualResult.Status == "REGISTERED" {
-					time.Sleep(time.Second * 1)
-					continue
+					break
 				}
 				if accrualResult.Status == "INVALID" || accrualResult.Status == "PROCESSED" {
 					procResult.Accrual = *accrualResult
 					res <- procResult
-					break
+					break worker_loop
 				}
 			}
+			timer.Stop()
 		}
 	}
 }
